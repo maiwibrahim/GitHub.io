77b23bc5feb22f58f0de7760b5e2cb18937d80e4
maiwibrahim <mai.w.ibrahim@gmail.com>
2026-03-29 22:33:48 +0000

    Deploying to gh-pages from @ maiwibrahim/maiwibrahim.github.io@7d89aa19ca8ac4af22c2696fb17d2a4327981cdd 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "maiwibrahim/GitHub.io",
+    "data-repo": "maiwibrahim/maiwibrahim.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
