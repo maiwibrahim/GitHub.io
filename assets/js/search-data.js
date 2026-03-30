2b7cdc316da2ef3c54b8d2a0d7836a71daeb75eb
maiwibrahim <mai.w.ibrahim@gmail.com>
2026-03-30 05:39:50 +0000

    Deploying to gh-pages from @ maiwibrahim/maiwibrahim.github.io@662e9927cc0bea8aea27f86030bd53aa1e195d30 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "research",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/research/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "cv",
           description: "",
           section: "Navigation",
